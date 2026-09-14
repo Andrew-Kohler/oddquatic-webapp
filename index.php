f3a55dc17b90c1beb88cfa50a0ab7990fe134cc0
Andrew Kohler <32870812+Andrew-Kohler@users.noreply.github.com>
2026-09-14 10:23:52 -0400

    First test of deploying database-related code

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
-<?php
+<!-- <?php
 
-echo "Hello World!";
+echo "Hello World!"; -->
+
+<?php
+    $serverName = "oddquatic-db-server-333.database.windows.net"; // update me
+    $connectionOptions = array(
+        "Database" => "oddquatic-resourcedb-333", // update me
+        "Uid" => "CloudSAe6d5ed3e", // update me
+        "PWD" => "xojoannaxo33+" // update me
+    );
+    //Establishes the connection
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    if( $conn === false ) {
+     die( print_r( sqlsrv_errors(), true));
+    }
+
+    $tsql= "SELECT * FROM dbo.Fish";
+    $getResults= sqlsrv_query($conn, $tsql);
+    echo ("Reading data from table" . PHP_EOL);
+    if ($getResults == FALSE)
+        echo (sqlsrv_errors());
+    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+     echo ($row['FishName'] . " " . $row['FishScale'] . PHP_EOL);
+    }
+    sqlsrv_free_stmt($getResults);
+?>
