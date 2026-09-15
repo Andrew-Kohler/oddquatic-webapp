e8c4f89496893fbb59a936fbff07e8d2aefd5601
Andrew Kohler <32870812+Andrew-Kohler@users.noreply.github.com>
2026-09-14 20:11:42 -0400

    Formatting fixes on INSERT command

--- inserts/insertFish.php
+++ inserts/insertFish.php
@@ -17,8 +17,9 @@
     // If our hash matches, our keys match, and we have verified that the request came from a legitimate client and we can proceed with insertion
     if($realHash == $_POST['hash'] ) { 
         // Set up our statement now that we understand what we're passing in
-        $tsql= "INSERT INTO dbo.Fish VALUES (:name, :scale, :color)";
+        $tsql= "INSERT INTO dbo.Fish VALUES (" . $_POST['name'] . ",". $_POST['scale'] . ",". $_POST['color'] . ")";
         $getResults= sqlsrv_query($conn, $tsql);
+
         if ($getResults == FALSE)
             echo (sqlsrv_errors());
     } 
